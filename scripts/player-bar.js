66d7f5f90f938b0aca0179d740b4efb964f18b88
Reisa Forster <r.forster10@gmail.com>
2020-06-09 15:19:55 -0400

    removed line

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -5,16 +5,13 @@
     $(this).on('click', function() {
       player.getDuration();
     })
-  $('#time-control .total-time').text( nextSong.duration );
   });
 
    $('button#next').on('click', function() {
     if (player.playState !== 'playing') { return; }
-
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
 
@@ -23,16 +20,12 @@
     $('#time-control .total-time').on('click', function() {
       player.getDuration();
     })
-
    });
-
    $('button#previous').on('click', function() {
     if (player.playState !== 'playing') { return; }
-
     const lastSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = lastSongIndex - 1;
     if (previousSongIndex >= album.songs.length) { return; }
-
     const previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
 
@@ -41,14 +34,10 @@
     $('#time-control .total-time').on('click', function() {
       player.getDuration();
     })
-
    });
-
-
    $('#time-control input').on('input', function (event) {
      player.skipTo(event.target.value);
    });
-
    setInterval( () => {
      if (player.playState !== 'playing') { return; }
      const currentTime = player.getTime();
@@ -57,11 +46,9 @@
      $('#time-control .current-time').text( currentTime );
      $('#time-control input').val(percent);
    }, 1000);
-
    $('#volume-control input').on('input', function (event) {
      player.setVolume(event.target.value);
    });
-
    setInterval( () => {
      if (player.playState !== 'playing') { return; }
      const currentVolume = player.getTime();
